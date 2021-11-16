453da1fa01eae496eb996903c87183947291e95e
gpotje <gpotje.souza@gmail.com>
2021-11-15 23:22:58 -0300

    aulas 9,10,11 e 12

--- src/main/java/com/example/controller/AnimeController.java
+++ src/main/java/com/example/controller/AnimeController.java
@@ -1,11 +1,17 @@
 package com.example.controller;
 
+import javax.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -29,10 +35,22 @@ public class AnimeController {
 
 	@GetMapping(path = "{id}")
 	public Mono<Anime> findByID(@PathVariable("id") Long id) {
-		return services.findById(id).switchIfEmpty(monoResponseStatusNotFoundException()).log();
+		return services.findById(id).switchIfEmpty(monoResponseStatusNotFoundException());
 	}
 
 	public <T> Mono<T> monoResponseStatusNotFoundException() {
 		return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
+	}
+
+	@PutMapping(path = "{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public Mono<Void> update(@PathVariable Long id, @Valid @RequestBody Anime anime) {
+		return services.update(anime.withId(id));
+	}
+
+	@DeleteMapping(path = "{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public Mono<Void> delete(@PathVariable Long id) {
+		return services.delete(id);
 	}
 }
--- src/main/java/com/example/services/AnimeServices.java
+++ src/main/java/com/example/services/AnimeServices.java
@@ -2,7 +2,9 @@ package com.example.services;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.domain.Anime;
 import com.example.repository.AnimeRepositor;
@@ -25,6 +27,23 @@ public class AnimeServices {
 		return repository.findById(id);
 
 	}
-	
+    public Mono<Anime> save(Anime anime) {
+        return repository.save(anime);
+    }
+    
+    public <T> Mono<T> monoResponseStatusNotFoundException() {
+        return Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Anime not found"));
+    }
+
+    public Mono<Void> update(Anime anime) {
+        return findById(anime.getId())
+            .flatMap(repository::save)
+            .then();
+    }
+
+    public Mono<Void> delete(Long id) {
+        return this.findById(id)
+            .flatMap(repository::delete);
+    }
 
 }
